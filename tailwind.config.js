1ca102178ee9ecd382f037ff698223a010647449
Harsh Kushwaha <harsh.kushwaha@partner.htdigital.in>
2024-06-22 01:22:45 +0530

    done

--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-   darkMode: "class",
-   content: ["./dist/*.html", "./dist/assets/js/*.js"],
-   theme: {
-      extend: {},
-   },
-   plugins: [],
-};
