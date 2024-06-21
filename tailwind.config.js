2f906e6f9d3826ad1d6ebad51b8abcd521162b37
Harsh Kushwaha <harsh.kushwaha@partner.htdigital.in>
2024-06-22 00:56:47 +0530

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+   darkMode: "class",
+   content: ["./dist/*.html", "./dist/assets/js/*.js"],
+   theme: {
+      extend: {},
+   },
+   plugins: [],
+};
