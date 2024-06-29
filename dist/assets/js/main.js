f32f39a62c57a739a407920e5a67b6c42fa01fba
Harsh Kushwaha <harsh.kushwaha@partner.htdigital.in>
2024-06-29 13:09:54 +0530

    Added animations

--- dist/assets/js/main.js
+++ dist/assets/js/main.js
@@ -2,7 +2,18 @@ const dropdown = document.getElementById("dropdown");
 const dropdownBtn = document.getElementById("dropdownBtn");
 const dropdownArrow = document.getElementById("dropdownArrow");
 const navbar = document.getElementById("navbar");
+const serviceIcon = document.getElementById("serviceIcon");
+const serviceMenu = document.getElementById("serviceMenu");
+const serviceOpen = document.getElementById("serviceOpen");
+const popup = document.getElementById("popup");
+const openPopup = document.getElementById("openPopup");
+const closePopup = document.getElementById("closePopup");
+const wrocustext = document.getElementById("wrocustext");
 const body = document.querySelector("body");
+const animatefromBottomNavs = document.querySelectorAll(
+  ".animatefromBottomNav"
+);
+const serviceTxtAnimates = document.querySelectorAll(".serviceTxtAnimate");
 let dropdownOpen = false;
 
 dropdownBtn.addEventListener("click", (e) => {
@@ -13,6 +24,10 @@ dropdownBtn.addEventListener("click", (e) => {
     dropdownArrow.classList.remove("rotate-180");
     body.classList.remove("h-screen", "overflow-hidden");
     dropdownOpen = false;
+    serviceTxtAnimates.forEach((serviceTxtAnimate) => {
+      serviceTxtAnimate.classList.add("translate-y-full", "opacity-0");
+      serviceTxtAnimate.style.transition = "0s";
+    });
   } else {
     dropdown.classList.remove("h-0");
     dropdown.classList.add("h-full");
@@ -20,12 +35,15 @@ dropdownBtn.addEventListener("click", (e) => {
     body.classList.add("h-screen", "overflow-hidden");
     dropdownArrow.classList.add("rotate-180");
     dropdownOpen = true;
+    serviceTxtAnimates.forEach((serviceTxtAnimate) => {
+      serviceTxtAnimate.classList.remove("translate-y-full", "opacity-0");
+      serviceTxtAnimate.style.transition = "1s";
+    });
   }
 });
 
 window.addEventListener("click", (e) => {
   let target = e.target;
-  console.log(target);
   if (target.classList.contains("closeDropdown")) {
     dropdown.classList.add("h-0");
     dropdown.classList.remove("h-full");
@@ -33,13 +51,93 @@ window.addEventListener("click", (e) => {
     dropdownArrow.classList.remove("rotate-180");
     body.classList.remove("h-screen", "overflow-hidden");
     dropdownOpen = false;
+    serviceTxtAnimates.forEach((serviceTxtAnimate) => {
+      serviceTxtAnimate.classList.add("translate-y-full", "opacity-0");
+      serviceTxtAnimate.style.transition = "0s";
+    });
   }
 });
 
 window.addEventListener("scroll", () => {
   if (window.scrollY > 100) {
-    navbar.classList.add("bg-white", "border-b", "border-zinc-200");
+    navbar.classList.add("bg-black");
   } else {
-    navbar.classList.remove("bg-white", "border-b", "border-zinc-200");
+    navbar.classList.remove("bg-black");
   }
+});
+
+let serviceMenuOpen = false;
+
+serviceOpen.addEventListener("click", () => {
+  if (serviceMenuOpen) {
+    serviceIcon.classList.remove("rotate-[135deg]");
+    serviceMenu.classList.add("h-0");
+    serviceMenuOpen = false;
+  } else {
+    serviceIcon.classList.add("rotate-[135deg]");
+    serviceMenu.classList.remove("h-0");
+    serviceMenuOpen = true;
+  }
+});
+
+openPopup.addEventListener("click", () => {
+  popup.classList.remove("h-0");
+  popup.classList.add("h-screen");
+  body.classList.add("h-screen", "overflow-hidden");
+  wrocustext.classList.remove("opacity-0");
+  wrocustext.style.transition = "1.5s";
+  animatefromBottomNavs.forEach((animatefromBottomNav) => {
+    animatefromBottomNav.classList.remove("opacity-0", "translate-y-full");
+    animatefromBottomNav.style.transition = "1s";
+  });
+});
+
+closePopup.addEventListener("click", () => {
+  popup.classList.add("h-0");
+  popup.classList.remove("h-screen");
+  body.classList.remove("h-screen", "overflow-hidden");
+  wrocustext.classList.add("opacity-0");
+  wrocustext.style.transition = "0s";
+  animatefromBottomNavs.forEach((animatefromBottomNav) => {
+    animatefromBottomNav.classList.add("opacity-0", "translate-y-full");
+    animatefromBottomNav.style.transition = "0s";
+  });
+});
+
+const animateFromBottoms = document.querySelectorAll(".animateFromBottom");
+const animateOpacityFulls = document.querySelectorAll(".animateOpacityFull");
+
+function animateFromBottom() {
+  animateFromBottoms.forEach((animateFromBottom) => {
+    const rect = animateFromBottom.getBoundingClientRect();
+    if (rect.top <= window.innerHeight && rect.bottom >= 0) {
+      animateFromBottom.classList.remove("translate-y-full", "opacity-0");
+      animateFromBottom.style.transition = "1s";
+    } else {
+      animateFromBottom.classList.add("translate-y-full", "opacity-0");
+      animateFromBottom.style.transition = "0s";
+    }
+  });
+}
+
+function animateOpacityFull() {
+  animateOpacityFulls.forEach((animateOpacityFull) => {
+    const rect = animateOpacityFull.getBoundingClientRect();
+    if (rect.top <= window.innerHeight && rect.bottom >= 0) {
+      animateOpacityFull.classList.remove("opacity-0");
+      animateOpacityFull.style.transition = "1.5s";
+    } else {
+      animateOpacityFull.classList.add("opacity-0");
+      animateOpacityFull.style.transition = "0s";
+    }
+  });
+}
+
+window.addEventListener("load", () => {
+  animateFromBottom();
+  animateOpacityFull();
+});
+window.addEventListener("scroll", () => {
+  animateFromBottom();
+  animateOpacityFull();
 });
